e48409206d952054ee7e96700ed21e131ec99e7d
Tuan Hung <>
2026-01-08 08:58:43 +0700

    - Add object highlight

---
+++ Assets/_Modules/TrickyTwist/Runtime/ScaleObjectHighlight.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using UnityEngine;
+
+public class ScaleObjectHighlight : MonoBehaviour
+{
+    [SerializeField] private Transform highlightTarget;
+    [SerializeField] private Vector3 maxScale = new Vector3(1.1f, 1.1f, 1);
+    [SerializeField] private float duration;
+    [SerializeField] private Ease ease;
+    [SerializeField] private bool enableHighlight = true;
+    private Tween scalingTween;
+
+    private void OnEnable()
+    {
+        if (!this.enableHighlight)
+        {
+            return;
+        }
+
+        this.scalingTween = this.highlightTarget.DOScale(this.maxScale, this.duration)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(this.ease);
+    }
+
+    private void OnDisable()
+    {
+        this.scalingTween?.Kill();
+    }
+}
