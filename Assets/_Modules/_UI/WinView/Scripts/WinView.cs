78c720d77780db61299e8fac01326665c04ed102
Tuan Hung <>
2026-01-16 16:34:43 +0700

    - Fade button win view

--- Assets/_Modules/_UI/WinView/Scripts/WinView.cs
+++ Assets/_Modules/_UI/WinView/Scripts/WinView.cs
@@ -27,9 +27,14 @@ namespace _Modules._UI.WinView.Scripts
         public override async void Show()
         {
             this.resultView.localScale = Vector3.zero;
+            this.continueButton.image.DOFade(0f, 0f);
+            this.replayButton.image.DOFade(0f, 0f);
             base.Show();
 
             await DOTween.Sequence().Append(this.resultView.DOScale(1f, 0.4f)).AsyncWaitForCompletion();
+            
+            this.continueButton.image.DOFade(1f, 0.5f);
+            this.replayButton.image.DOFade(1f, 0.5f);
         }
     }
 }
