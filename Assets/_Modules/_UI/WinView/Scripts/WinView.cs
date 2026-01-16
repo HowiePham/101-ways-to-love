82d7e169ceb334262d0ded92c3f9473ecb7bbde8
Tuan Hung <>
2026-01-16 16:29:31 +0700

    - Add scaling effect for win view

--- Assets/_Modules/_UI/WinView/Scripts/WinView.cs
+++ Assets/_Modules/_UI/WinView/Scripts/WinView.cs
@@ -1,4 +1,5 @@
 using System;
+using DG.Tweening;
 using Mimi.Prototypes.UI;
 using TMPro;
 using UnityEngine;
@@ -8,6 +9,7 @@ namespace _Modules._UI.WinView.Scripts
 {
     public class WinView : BaseView
     {
+        [SerializeField] private RectTransform resultView;
         [SerializeField] private Button continueButton;
         [SerializeField] private Button replayButton;
 
@@ -20,6 +22,14 @@ namespace _Modules._UI.WinView.Scripts
 
             this.continueButton.onClick.AddListener(() => OnContinueClicked?.Invoke());
             this.replayButton.onClick.AddListener(() => OnReplayClicked?.Invoke());
+        }
+
+        public override async void Show()
+        {
+            this.resultView.localScale = Vector3.zero;
+            base.Show();
+
+            await DOTween.Sequence().Append(this.resultView.DOScale(1f, 0.4f)).AsyncWaitForCompletion();
         }
     }
 }
