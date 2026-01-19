3e5b11d65de82a669fb8e56b449731b46f73ec80
Tuan Hung <>
2026-01-19 09:39:09 +0700

    - Fade button group

--- Assets/_Modules/_UI/WinView/Scripts/WinView.cs
+++ Assets/_Modules/_UI/WinView/Scripts/WinView.cs
@@ -1,7 +1,6 @@
 using System;
 using DG.Tweening;
 using Mimi.Prototypes.UI;
-using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,6 +9,8 @@ namespace _Modules._UI.WinView.Scripts
     public class WinView : BaseView
     {
         [SerializeField] private RectTransform resultView;
+        [SerializeField] private CanvasGroup continueBtnGroup;
+        [SerializeField] private CanvasGroup replayBtnGroup;
         [SerializeField] private Button continueButton;
         [SerializeField] private Button replayButton;
 
@@ -27,14 +28,15 @@ namespace _Modules._UI.WinView.Scripts
         public override async void Show()
         {
             this.resultView.localScale = Vector3.zero;
-            this.continueButton.image.DOFade(0f, 0f);
-            this.replayButton.image.DOFade(0f, 0f);
+            this.continueBtnGroup.DOFade(0f, 0f);
+            this.replayBtnGroup.DOFade(0f, 0f);
+
             base.Show();
 
             await DOTween.Sequence().Append(this.resultView.DOScale(1f, 0.4f)).AsyncWaitForCompletion();
-            
-            this.continueButton.image.DOFade(1f, 0.5f);
-            this.replayButton.image.DOFade(1f, 0.5f);
+
+            this.continueBtnGroup.DOFade(1f, 0.5f);
+            this.replayBtnGroup.DOFade(1f, 0.5f);
         }
     }
 }
