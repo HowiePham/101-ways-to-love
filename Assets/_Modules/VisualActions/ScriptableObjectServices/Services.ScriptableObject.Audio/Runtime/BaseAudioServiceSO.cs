79de46aa68d7e570d3eb212c59149f44f7adcf8f
Tuan Hung <>
2026-02-05 10:47:43 +0700

    - Rehandle audio system

--- Assets/_Modules/VisualActions/ScriptableObjectServices/Services.ScriptableObject.Audio/Runtime/BaseAudioServiceSO.cs
+++ Assets/_Modules/VisualActions/ScriptableObjectServices/Services.ScriptableObject.Audio/Runtime/BaseAudioServiceSO.cs
@@ -9,8 +9,6 @@ namespace Mimi.Services.ScriptableObject.Audio
     {
         protected IAudioPlayer wrapAudioPlayer;
         protected virtual IAudioPlayer WrapAudioPlayer => wrapAudioPlayer;
-        protected float soundVolPercentage = 1f;
-        protected float musicVolPercentage = 1f;
 
         public override UniTask Initialize()
         {
@@ -23,24 +21,17 @@ namespace Mimi.Services.ScriptableObject.Audio
         public void PlaySound(string key, float volumePercentage = 1, float pitch = 1)
         {
             WrapAudioPlayer?.PlaySound(key, volumePercentage, pitch);
-            SetSoundVolPercentage(this.soundVolPercentage);
-            SetMusicVolPercentage(this.musicVolPercentage);
+
         }
 
         public void SetSoundVolPercentage(float percentage)
         {
             WrapAudioPlayer?.SetSoundVolPercentage(percentage);
-            this.soundVolPercentage = percentage;
-            
-            Debug.Log($"--- (AUDIO) New sound vol: {this.soundVolPercentage}");
         }
 
         public void SetMusicVolPercentage(float percentage)
         {
             WrapAudioPlayer?.SetMusicVolPercentage(percentage);
-            this.musicVolPercentage = percentage;
-            
-            Debug.Log($"--- (AUDIO) New Music vol: {this.musicVolPercentage}");
         }
 
         public abstract void StopSound(string key);
--- Assets/_Modules/_Game/Scripts/ServiceFactory/AudioServiceAdapter.cs
+++ Assets/_Modules/_Game/Scripts/ServiceFactory/AudioServiceAdapter.cs
@@ -1,4 +1,5 @@
 using Mimi.Audio;
+using UnityEngine;
 
 namespace Mimi.Prototypes
 {
@@ -6,6 +7,8 @@ namespace Mimi.Prototypes
     {
         public float SoundVolPercentage => this.audioPlayer.SoundVolPercentage;
         public float MusicVolPercentage => this.audioPlayer.MusicVolPercentage;
+        protected float soundVolPercentage = 1f;
+        protected float musicVolPercentage = 1f;
 
         private readonly IAudioPlayer audioPlayer;
 
@@ -17,16 +20,24 @@ namespace Mimi.Prototypes
         public void PlaySound(string key, float volumePercentage = 1, float pitch = 1)
         {
             this.audioPlayer.PlaySound(key, volumePercentage, pitch);
+            SetSoundVolPercentage(this.soundVolPercentage);
+            SetMusicVolPercentage(this.musicVolPercentage);
         }
 
         public void SetSoundVolPercentage(float percentage)
         {
             this.audioPlayer.SetSoundVolPercentage(percentage);
+            this.soundVolPercentage = percentage;
+
+            Debug.Log($"--- (AUDIO) New sound vol: {this.soundVolPercentage}");
         }
 
         public void SetMusicVolPercentage(float percentage)
         {
             this.audioPlayer.SetMusicVolPercentage(percentage);
+            this.musicVolPercentage = percentage;
+
+            Debug.Log($"--- (AUDIO) New Music vol: {this.musicVolPercentage}");
         }
     }
 }
