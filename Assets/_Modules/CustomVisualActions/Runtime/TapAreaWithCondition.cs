1a7c9fd95502298d1693fc02e690523a83588ed1
Tuan Hung <>
2026-02-10 20:34:28 +0700

    - Handle Tap Area With Condition

---
+++ Assets/_Modules/CustomVisualActions/Runtime/TapAreaWithCondition.cs
@@ -0,0 +1,24 @@
+﻿using Lean.Touch;
+using Mimi.VisualActions;
+using Mimi.VisualActions.Tapping;
+using UnityEngine;
+
+public class TapAreaWithCondition : TapArea
+{
+    [SerializeField] private VisualCondition condition;
+
+    protected override void FingerTapHandler(LeanFinger finger)
+    {
+        if (!this.Target.Active || finger.IsOverGui)
+        {
+            return;
+        }
+
+        if (!this.Target.ContainsScreenPosition(finger.ScreenPosition, Camera.main) || !this.condition.Validate())
+        {
+            return;
+        }
+
+        this.complete = true;
+    }
+}
