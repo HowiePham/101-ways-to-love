d42e9a2606bdb78eba4dd24eb1c6b4b3264de1fd
Tuan Hung <>
2026-02-10 10:53:06 +0700

    - Add L_064

---
+++ Assets/_Modules/CustomVisualActions/Runtime/FollowHand.cs
@@ -0,0 +1,50 @@
+using System;
+using Lean.Touch;
+using Mimi.VisualActions;
+using UnityEngine;
+
+public class FollowHand : MonoBehaviour
+{
+    [SerializeField] private VisualAction actionCondition;
+    private bool isDragging;
+
+    private void Start()
+    {
+        LeanTouch.OnFingerOld += StartDragging;
+        LeanTouch.OnFingerUp += StopDragging;
+        LeanTouch.OnFingerUpdate += MoveFollowHand;
+    }
+
+    private void OnDisable()
+    {
+        LeanTouch.OnFingerOld -= StartDragging;
+        LeanTouch.OnFingerUp -= StopDragging;
+        LeanTouch.OnFingerUpdate -= MoveFollowHand;
+    }
+
+    private void MoveFollowHand(LeanFinger finger)
+    {
+        if (!this.isDragging || this.actionCondition.Completed)
+        {
+            return;
+        }
+
+        this.transform.position = finger.GetWorldPosition(10f, Camera.main);
+    }
+
+    private void StopDragging(LeanFinger finger)
+    {
+        this.isDragging = false;
+    }
+
+    private void StartDragging(LeanFinger finger)
+    {
+        if (finger.IsOverGui)
+        {
+            this.isDragging = false;
+            return;
+        }
+
+        this.isDragging = true;
+    }
+}
