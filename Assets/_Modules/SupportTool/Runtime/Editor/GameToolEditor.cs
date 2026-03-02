93f721b425fb3bc85aaaac8d9ac68ef64d86917b
Tuan Hung <>
2026-03-02 16:40:40 +0700

    - Rehandle 101LovePuzzle editor tool

--- Assets/_Modules/SupportTool/Runtime/Editor/GameToolEditor.cs
+++ Assets/_Modules/SupportTool/Runtime/Editor/GameToolEditor.cs
@@ -3,19 +3,19 @@ using UnityEditor;
 
 public class GameToolEditor
 {
-    [MenuItem("GameObject/Visual Actions/101WaysToLove/Level Blueprint")]
+    [MenuItem("101WaysToLove/Level Blueprint")]
     public static void CreateLevelBlueprint(MenuCommand menuCommand)
     {
         LevelBlueprintEditorWindow.ShowWindow();
     }
 
-    [MenuItem("GameObject/Visual Actions/101WaysToLove/Mechanic")]
+    [MenuItem("101WaysToLove/Mechanic")]
     public static void CreateMechanicBlueprint(MenuCommand menuCommand)
     {
         MechanicBlueprintEditorWindow.ShowWindow();
     }
 
-    [MenuItem("Assets/Create/Project Structure/Level Folder")]
+    [MenuItem("101WaysToLove/Project Structure/Level Folder")]
     public static void CreateLevelFolder(MenuCommand menuCommand)
     {
         LevelFolderCreatorWindow.ShowWindow();
