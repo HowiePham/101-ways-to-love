d78559b4cc0aa703f750bcb897fdbbd6d04450b7
Tuan Hung <>
2026-03-16 21:25:01 +0700

    - Handle L_083 logic

---
+++ Assets/_Modules/CustomVisualActions/Runtime/CheckTotalValue.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using Mimi.VisualActions;
+using UnityEngine;
+
+public class CheckTotalValue : VisualAction
+{
+    [SerializeField] private TotalValue totalValue;
+    [SerializeField] private float targetValue;
+    [SerializeField] private bool equal = true;
+    [SerializeField] private bool larger;
+
+    protected override async UniTask OnExecuting(CancellationToken cancellationToken)
+    {
+        if (this.totalValue == null)
+        {
+            return;
+        }
+
+        if (this.equal)
+        {
+            await UniTask.WaitUntil(() => Mathf.Approximately(this.totalValue.Total, this.targetValue), cancellationToken: cancellationToken);
+        }
+        else if (this.larger)
+        {
+            await UniTask.WaitUntil(() => this.totalValue.Total > this.targetValue, cancellationToken: cancellationToken);
+        }
+        else
+        {
+            await UniTask.WaitUntil(() => this.totalValue.Total < this.targetValue, cancellationToken: cancellationToken);
+        }
+    }
+}
---
+++ Assets/_Modules/CustomVisualActions/Runtime/SetTotalValue.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using Mimi.VisualActions;
+using UnityEngine;
+
+public class SetTotalValue : VisualAction
+{
+    [SerializeField] private TotalValue totalValue;
+    [SerializeField] private float value;
+
+    protected override async UniTask OnExecuting(CancellationToken cancellationToken)
+    {
+        this.totalValue.SetTotalValue(this.value);
+    }
+}
