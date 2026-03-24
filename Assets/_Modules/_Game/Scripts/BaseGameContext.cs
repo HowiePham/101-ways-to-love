b60eab5665c6e4cd308926c2c5cf1b0f95cb94af
Tuan Hung <>
2026-03-24 12:41:03 +0700

    - Disable firebase

--- Assets/_Modules/_Game/Scripts/BaseGameContext.cs
+++ Assets/_Modules/_Game/Scripts/BaseGameContext.cs
@@ -172,7 +172,8 @@ namespace Mimi.Prototypes
 
         private void CreateAnalyticService()
         {
-            AnalyticTracker = new ReflectionTracker(new FirebaseTrackingProvider());
+            // AnalyticTracker = new ReflectionTracker(new FirebaseTrackingProvider());
+            AnalyticTracker = new NullTracker();
 
 #if UNITY_EDITOR
             AnalyticTracker = new NullTracker();
@@ -276,7 +277,7 @@ namespace Mimi.Prototypes
                 return;
             }
 
-            FirebaseAnalytics.LogEvent(eventName);
+            // FirebaseAnalytics.LogEvent(eventName);
         }
 
         private async UniTask InitGoogleMobileAds()
@@ -354,17 +355,17 @@ namespace Mimi.Prototypes
                     {
                         Debug.Log($"--- (ADS) Admob Banner Ads initializing...");
                         Ads.SetBanner(new AutoRequestBanner(bannerRequestStrategy,
-                            new FirebaseMeasureRevenueBanner(
-                                new SingularRevenueBanner(
-                                    new AdmobBanner(AdmobBannerId)))));
+                            // new FirebaseMeasureRevenueBanner(
+                            new SingularRevenueBanner(
+                                new AdmobBanner(AdmobBannerId))));
                     }
                     else
                     {
                         Ads.SetBanner(new AutoRequestBanner(bannerRequestStrategy,
-                            new FirebaseMeasureRevenueBanner(
-                                new SingularRevenueBanner(
-                                    new AmazonMaxBanner(TabletAmazonBannerId,
-                                        PhoneAmazonUnitId, MaxBannerUnitId)))));
+                            // new FirebaseMeasureRevenueBanner(
+                            new SingularRevenueBanner(
+                                new AmazonMaxBanner(TabletAmazonBannerId,
+                                    PhoneAmazonUnitId, MaxBannerUnitId))));
                     }
                 }
                 else
@@ -378,9 +379,9 @@ namespace Mimi.Prototypes
 
                     Ads.SetInterstitial(
                         new AutoRequestInterstitial(interstitialRequestStrategy,
-                            new FirebaseMeasureRevenueInterstitial(
-                                new SingularRevenueInterstitial(
-                                    new MaxInterstitial(MaxInterUnityId)))));
+                            // new FirebaseMeasureRevenueInterstitial(
+                            new SingularRevenueInterstitial(
+                                new MaxInterstitial(MaxInterUnityId))));
                 }
                 else
                 {
@@ -404,17 +405,17 @@ namespace Mimi.Prototypes
 
                     Ads.SetAppOpen(
                         new AutoRequestAppOpen(appOpenRequestStrategy,
-                            new FirebaseMeasureRevenueAppOpen(
-                                new SingularRevenueAppOpen(
-                                    new MaxAppOpen(MaxAOAUnitId)))));
+                            // new FirebaseMeasureRevenueAppOpen(
+                            new SingularRevenueAppOpen(
+                                new MaxAppOpen(MaxAOAUnitId))));
                 }
                 else
                 {
                     Ads.SetAppOpen(
                         new AutoRequestAppOpen(appOpenRequestStrategy,
-                            new FirebaseMeasureRevenueAppOpen(
-                                new SingularRevenueAppOpen(
-                                    new AdmobAppOpen(AdmobAOAUnitId)))));
+                            // new FirebaseMeasureRevenueAppOpen(
+                            new SingularRevenueAppOpen(
+                                new AdmobAppOpen(AdmobAOAUnitId))));
                 }
             }
             else
@@ -432,9 +433,9 @@ namespace Mimi.Prototypes
                 var rewardVideoRequestStrategy = new ExponentialCooldown(999, 2, InternetMonitor);
                 Ads.SetRewardVideo(
                     new AutoRequestRewardVideo(rewardVideoRequestStrategy,
-                        new FirebaseMeasureRevenueRewardVideo(
-                            new SingularRevenueRewardVideo(
-                                new MaxRewardVideo(MaxRewardUnitId)))));
+                        // new FirebaseMeasureRevenueRewardVideo(
+                        new SingularRevenueRewardVideo(
+                            new MaxRewardVideo(MaxRewardUnitId))));
             }
             else
             {
@@ -472,7 +473,7 @@ namespace Mimi.Prototypes
                 new Parameter(FirebaseAnalytics.ParameterCurrency, "USD"),
                 new Parameter(FirebaseAnalytics.ParameterValue, impressionData.Revenue)
             };
-            FirebaseAnalytics.LogEvent("ad_impression_mediation", parameters);
+            // FirebaseAnalytics.LogEvent("ad_impression_mediation", parameters);
         }
 
         private void CreateMrecWithCustomPosition()
@@ -483,8 +484,8 @@ namespace Mimi.Prototypes
             var mrecRequestStrategy = new ExponentialCooldown(999, 2, InternetMonitor);
 
             Ads.SetMrec(new AutoRequestMrec(mrecRequestStrategy,
-                new FirebaseAdRevenueMrec(
-                    new SingularRevenueMrec(this.maxMrec))));
+                // new FirebaseAdRevenueMrec(
+                new SingularRevenueMrec(this.maxMrec)));
         }
 
         private void CalculateMrecPos()
