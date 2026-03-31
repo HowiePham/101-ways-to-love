8f616246535f175066a6af3a65ef8f1abf831bd9
Tuan Hung <>
2026-03-31 14:36:12 +0700

    - Fix first session count

--- Assets/_Modules/_Game/Scripts/GameContext.cs
+++ Assets/_Modules/_Game/Scripts/GameContext.cs
@@ -52,8 +52,8 @@ namespace Mimi.Prototypes
         protected override void AddGlobalPlugins(CompositePlugin pluginInstaller)
         {
             pluginInstaller.AddPlugin(new LoadFirstAdsPlugin(this));
-            pluginInstaller.AddPlugin(new ShowOpenAdAfterBootPlugin(this.Ads, this.EventSubscriber, this.RemoteConfig, this.SessionRecorder));
-            pluginInstaller.AddPlugin(new ShowOpenAdOnResumePlugin(this.Ads, this.EventSubscriber, this.IsRemoveAds, this.SessionRecorder, this.RemoteConfig));
+            pluginInstaller.AddPlugin(new ShowOpenAdAfterBootPlugin(this.Ads, this.EventSubscriber, this.RemoteConfig, this.IsFirstSession));
+            pluginInstaller.AddPlugin(new ShowOpenAdOnResumePlugin(this.Ads, this.EventSubscriber, this.IsRemoveAds, this.RemoteConfig, this.IsFirstSession));
             pluginInstaller.AddPlugin(new LogHintLevelShow(this.RuntimeState, this.EventSubscriber, this.AnalyticTracker));
             pluginInstaller.AddPlugin(new LogLevelStartPlugin(this.RuntimeState, this.EventSubscriber, this.AnalyticTracker));
             pluginInstaller.AddPlugin(new LogLevelCompletedPlugin(this.RuntimeState, this.EventSubscriber, this.AnalyticTracker));
