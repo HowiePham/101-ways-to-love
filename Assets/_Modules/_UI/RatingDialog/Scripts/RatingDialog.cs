ca95c03bf1c2105c0fa45f001cc8f3f41def786e
Tuan Hung <>
2026-04-14 10:06:19 +0700

    - Fix rating dialog cannot be closed

--- Assets/_Modules/_UI/RatingDialog/Scripts/RatingDialog.cs
+++ Assets/_Modules/_UI/RatingDialog/Scripts/RatingDialog.cs
@@ -1,26 +1,48 @@
-using System;
+using Mimi.Prototypes.Pooling;
 using Mimi.Prototypes.UI;
+using Mimi.ServiceLocators;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class RatingDialog : BaseModalDialog
+public class RatingDialog : SimpleAnimModalDialog
 {
     [SerializeField] private Button submitBut;
     [SerializeField] private Button laterBut;
     [SerializeField] private TextMeshProUGUI tmTittle;
 
-    public override void Show()
+    [SerializeField] private UnityEvent onClickYesEvent;
+    [SerializeField] private UnityEvent onClickNoEvent;
+
+    private void Awake()
     {
-        base.Show();
         this.submitBut.onClick.AddListener(AcceptRating);
-        this.laterBut.onClick.AddListener(Hide);
+        this.laterBut.onClick.AddListener(OnClickNo);
     }
 
     private void AcceptRating()
     {
-        Hide();
         RateMarket();
+        this.onClickYesEvent.Invoke();
+        Hide();
+    }
+
+    public void SetYesCallback(UnityAction action)
+    {
+        this.onClickYesEvent.AddListener(action);
+    }
+
+    public void SetNoCallback(UnityAction action)
+    {
+        this.onClickNoEvent.AddListener(action);
+    }
+
+    private void OnClickNo()
+    {
+        PlayerPrefs.SetInt("isAppRated", 1);
+        this.onClickNoEvent.Invoke();
+        Hide();
     }
 
     private void RateMarket()
@@ -36,7 +58,13 @@ public class RatingDialog : BaseModalDialog
 
     public override void Hide()
     {
-        PlayerPrefs.SetInt("isAppRated", 1);
+        this.onClickYesEvent.RemoveAllListeners();
+        this.onClickNoEvent.RemoveAllListeners();
         base.Hide();
+    }
+
+    protected override void OnHideComplete()
+    {
+        ServiceLocator.Global.Get<IPoolService>().Despawn(gameObject);
     }
 }
