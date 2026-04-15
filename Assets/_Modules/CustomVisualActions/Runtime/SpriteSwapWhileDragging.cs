4c02e7f7ca31f9b4cfb97737f9a5fb013011abcf
Tuan Hung <>
2026-04-15 10:32:29 +0700

    - Rehandle sprite swap function

--- Assets/_Modules/CustomVisualActions/Runtime/SpriteSwapWhileDragging.cs
+++ Assets/_Modules/CustomVisualActions/Runtime/SpriteSwapWhileDragging.cs
@@ -1,33 +1,33 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using Mimi.Interactions.Dragging;
 using Mimi.Interactions.Dragging.DraggableExtensions;
 using UnityEngine;
 
 public class SpriteSwapWhileDragging : MonoDraggableExtension
 {
-    [SerializeField] private SpriteRenderer spriteRenderer;
     [SerializeField] private Sprite swappedSprite;
-    private Sprite originSprite;
+    [SerializeField] private SpriteRenderer itemSprite;
+    [SerializeField] private float changeDelay;
+    [SerializeField] private float rollbackDelay;
+
+    private Sprite oldSprite;
+    private CancellationTokenSource cts;
 
     public override void Init(BaseDraggable draggable)
     {
         base.Init(draggable);
 
-        if (this.spriteRenderer == null)
+        if (this.itemSprite != null)
         {
-            return;
+            this.oldSprite = this.itemSprite.sprite;
         }
-
-        this.originSprite = this.spriteRenderer.sprite;
     }
 
     public override void StartDrag()
     {
-        if (this.spriteRenderer == null || this.swappedSprite == null)
-        {
-            return;
-        }
-
-        this.spriteRenderer.sprite = this.swappedSprite;
+        ResetCts();
+        ChangeSprite(this.swappedSprite, this.changeDelay, this.cts.Token).Forget();
     }
 
     public override void Drag()
@@ -36,11 +36,29 @@ public class SpriteSwapWhileDragging : MonoDraggableExtension
 
     public override void EndDrag()
     {
-        if (this.spriteRenderer == null)
-        {
-            return;
-        }
+        ResetCts();
+        ChangeSprite(this.oldSprite, this.rollbackDelay, this.cts.Token).Forget();
+    }
 
-        this.spriteRenderer.sprite = this.originSprite;
+    private void OnDestroy()
+    {
+        this.cts?.Cancel();
+        this.cts?.Dispose();
+    }
+
+    private void ResetCts()
+    {
+        this.cts?.Cancel();
+        this.cts?.Dispose();
+        this.cts = new CancellationTokenSource();
+    }
+
+    private async UniTask ChangeSprite(Sprite newSprite, float delay, CancellationToken cancellationToken)
+    {
+        if (this.itemSprite != null)
+        {
+            await UniTask.WaitForSeconds(delay, cancellationToken: cancellationToken);
+            this.itemSprite.sprite = newSprite;
+        }
     }
 }
