65b173cfb6fbcf0f8bd667e26671bff91ab9b2b0
Tuan Hung <>
2026-05-03 12:28:16 +0700

    - Change load_time param in loading_finish event from string to int

---
+++ Assets/_Modules/GameEvent/Scripts/LoadingFinishEventData.cs
@@ -0,0 +1,15 @@
+﻿using Mimi.Analytics.Tracking.Trackers;
+
+public struct LoadingFinishEventData : IEventData
+{
+    public enum EVENT_NAME
+    {
+        loading_finish
+    }
+
+    public EVENT_NAME eventName { get; set; }
+    public string placement { get; set; }
+    public string is_load { get; set; }
+    public int load_time { get; set; }
+    public string is_remote_config_loaded { get; set; }
+}
